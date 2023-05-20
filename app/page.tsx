53bd0f381881a7de60a34dcfe3018dc427703d0e
hdangnguyen <hdangprod@gmail.com>
2023-05-20 18:11:42 +0700

    restructure project

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My Page Title",
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>My Page</h1>
+    </>
+  );
+}
