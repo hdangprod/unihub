14a5358b3ddafff2e1e08a2fe2ee096c40989db3
hdangnguyen <hdangprod@gmail.com>
2023-05-23 15:39:50 +0700

    initialize app

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ interface IAuthLayoutProps {
 export default function AuthLayout({ children }: IAuthLayoutProps) {
   return (
     <div className="flex flex-row">
-      <div className="basis-4/12 bg-red-300">{children}</div>
-      <div className="basis-6/12 bg-cyan-200">hello</div>
+      <div className="bg-red-300 md:basis-5/12">{children}</div>
+      <div className="bg-cyan-200 md:basis-7/12">fhjdkh</div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,7 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
