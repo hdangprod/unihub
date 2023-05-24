29d03591571da86d352d09164fa341c25155c242
hdangnguyen <hdangprod@gmail.com>
2023-05-25 03:33:20 +0700

    add gap

--- src/components/sidebarItem/index.tsx
+++ src/components/sidebarItem/index.tsx
@@ -9,7 +9,7 @@ interface ISidebarItemProps {
 export default function SidebarItem({ href, title, icon }: ISidebarItemProps) {
   return (
     <div className="group h-full w-full rounded-xl py-4 hover:bg-sky-50 hover:text-slate-200">
-      <Link className="flex  flex-col items-center gap-1 " href={href}>
+      <Link className="flex  flex-col items-center gap-2 " href={href}>
         <HeroIcon icon={icon} className="h-6 w-6 text-sky-400 " />
         <h1 className="text-center text-xs text-slate-500 group-hover:text-sky-400">
           {title}
