d158143270f856960c95a4a6a691cb202fc9887b
hdangnguyen <hdangprod@gmail.com>
2023-05-25 04:20:07 +0700

    fix position

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import logo from "@/public/assets/images/logo.png";
+
 import loginImg from "@/public/assets/images/login-screen.jpeg";
 
 interface IAuthLayoutProps {
@@ -9,6 +11,15 @@ export default function AuthLayout({ children }: IAuthLayoutProps) {
     <div className="flex h-screen flex-row ">
       <div className="hidden md:relative md:flex md:basis-6/12 lg:basis-7/12 ">
         <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black via-black to-transparent opacity-40" />
+        <div>
+          <Image
+            className="absolute left-1/4 top-[14%] -translate-x-1/2 brightness-0 invert lg:left-[14%]"
+            src={logo}
+            height={40}
+            alt="logo"
+            priority={true}
+          />
+        </div>
         <div className="absolute bottom-1/4 left-1/4 flex w-2/3 -translate-x-1/4 flex-col gap-3 p-4 text-white  ">
           <h1 className="mb-2 text-4xl font-bold">Minimize your distraction</h1>
           <p>
