bf3fec7fbbbff6bfa8135f4a2bf298058a104934
hdangnguyen <hdangprod@gmail.com>
2023-05-25 03:52:07 +0700

    change description text

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -9,9 +9,13 @@ export default function AuthLayout({ children }: IAuthLayoutProps) {
     <div className="flex h-screen flex-row md:overflow-hidden">
       <div className="hidden md:relative md:flex md:basis-6/12 lg:basis-7/12 ">
         <div className="absolute inset-0 rounded-xl bg-gradient-to-t from-black via-black to-transparent opacity-40" />
-        <div className="absolute bottom-1/4 p-4 text-white md:left-1/4 lg:-translate-x-1/2 ">
-          <h1 className="mb-4 text-4xl font-bold">Mint</h1>
-          <p className="text-lg font-semibold">Minimize your distraction</p>
+        <div className="absolute bottom-1/4 left-1/4 flex w-2/3 -translate-x-1/4 flex-col gap-3 p-4 text-white  ">
+          <h1 className="mb-2 text-4xl font-bold">Minimize your distraction</h1>
+          <p className="text-lg">
+            Study Together, in a virtual community spread around all continents,
+            we are making it possible for learners around the world to connect,
+            learn, teach, share, support and help each other.
+          </p>
         </div>
         <Image
           src={loginImg}
