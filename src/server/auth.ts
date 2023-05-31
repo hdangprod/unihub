7544613f7d3e077ea1d4ae487e1450f799de5031
hdangnguyen <hdangprod@gmail.com>
2023-06-01 00:31:55 +0700

    connect to prisma

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,13 +1,18 @@
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { env } from "@/env/env.mjs";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
+const prisma = new PrismaClient();
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
   pages: {
     signIn: "/login",
+    signOut: "/login",
   },
   providers: [
     GoogleProvider({
