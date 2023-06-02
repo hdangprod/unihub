e3c0ab584391a2c7d96591417007692d1ffc6d96
hdangnguyen <hdangprod@gmail.com>
2023-06-02 09:25:19 +0700

    format code

--- src/provider/authProvider.tsx
+++ src/provider/authProvider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode } from "react";
+import React, { type ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 interface Props {
   children: ReactNode;
