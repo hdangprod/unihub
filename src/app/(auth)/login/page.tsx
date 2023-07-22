15299ed4dff78e663e6695aab358d77cd44a92e7
hdangnguyen <hdangprod@gmail.com>
2023-07-22 17:43:25 +0700

    suspend

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,22 @@
 import GoogleButton from "@/components/google-button";
 import Link from "next/link";
+import { Suspense } from "react";
+
+function SearchBarFallback() {
+  return <>Loading</>;
+}
 
 export default function Login() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-7">
       <h1 className="text-4xl font-bold text-slate-800">Login</h1>
-      <GoogleButton />
+      <Suspense fallback={<SearchBarFallback />}>
+        <GoogleButton />
+      </Suspense>
+
       <div className="text-slate-500 ">
         New user ?
         <Link className="text-amber-400" href="/register">
-          {" "}
           Create an account
         </Link>
       </div>
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,15 +1,21 @@
 import GoogleButton from "@/components/google-button";
 import Link from "next/link";
+import { Suspense } from "react";
+
+function SearchBarFallback() {
+  return <>Loading</>;
+}
 
 export default function Register() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-7">
       <h1 className="text-4xl font-bold text-slate-800">Create an account</h1>
-      <GoogleButton />
+      <Suspense fallback={<SearchBarFallback />}>
+        <GoogleButton />
+      </Suspense>
       <div className="text-slate-500 ">
         You have an account ?
         <Link className="text-amber-400" href="/login">
-          {" "}
           Login
         </Link>
       </div>
