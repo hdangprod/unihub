2774cdc50c9fda63e1ed720fe24c9816595da6e4
hdangnguyen <hdangprod@gmail.com>
2023-07-25 00:55:53 +0700

    test deployment

--- src/app/(home)/group/[groupId]/page.tsx
+++ src/app/(home)/group/[groupId]/page.tsx
@@ -1,4 +1,11 @@
-import GroupIdComp from "@/components/groupid";
+"use client";
+import { api } from "@/utils/api";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+
+const VideoCall = dynamic(() => import("@/components/video-call"), {
+  ssr: false,
+});
 
 interface IGroupIdProps {
   params: {
@@ -6,12 +13,36 @@ interface IGroupIdProps {
   };
 }
 
+export const dynamicParams = true; // true | false,
+
+export function generateStaticParams() {
+  const { data: channels } = api.channelRouter.getAll.useQuery();
+  return channels?.map((channel) => ({ groupId: channel.id })) || [];
+}
 export default function GroupId({ params }: IGroupIdProps) {
+  const [inCall, setInCall] = useState(false);
+
+  const { data: channelTokenRouter } = api.channelRouter.getToken.useQuery({
+    channelId: params.groupId,
+  });
+
   return (
     <div>
-      <div>
-        <GroupIdComp params={params} />
-      </div>
+      {inCall ? (
+        <VideoCall
+          uid={channelTokenRouter?.account as string}
+          token={channelTokenRouter?.token as string}
+          channelName={params.groupId}
+          setInCall={setInCall}
+        />
+      ) : (
+        <button
+          className=" rounded bg-sky-400 p-4 text-white"
+          onClick={() => setInCall(true)}
+        >
+          Start Call
+        </button>
+      )}
     </div>
   );
 }
