2f2021020a972f30866c98dcbbb3aa752b6491eb
hdangnguyen <hdangprod@gmail.com>
2023-07-26 22:22:03 +0700

    h

--- src/app/(home)/group/[groupId]/page.tsx
+++ src/app/(home)/group/[groupId]/page.tsx
@@ -1,5 +1,5 @@
 import Test4 from "@/components/test4";
-import { fetchAllChannels } from "@/server/handlers/fetchAllChannels";
+// import { fetchAllChannels } from "@/server/handlers/fetchAllChannels";
 import { Suspense } from "react";
 
 interface IGroupIdProps {
@@ -7,19 +7,20 @@ interface IGroupIdProps {
     groupId: string;
   };
 }
-export async function generateStaticParams() {
-  const channels = await fetchAllChannels();
-  return channels.map((channel) => {
-    return {
-      groupId: channel.id,
-    };
-  });
-}
+// export async function generateStaticParams() {
+//   const channels = await fetchAllChannels();
+//   return channels.map((channel) => {
+//     return {
+//       groupId: channel.id,
+//     };
+//   });
+// }
 
-export default function GroupId({ params }: IGroupIdProps) {
+export default async function GroupId({ params }: IGroupIdProps) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return (
     <div>
-      <h1>Group is {params.groupId}</h1>
+      <h1>Group {params.groupId}</h1>
       <Suspense fallback={<div>Loading....</div>}>
         <Test4 params={params} />
       </Suspense>
